7c6390c3f384afc834f15a2ded0d2ad5f7cec645
Diwanshu Suman <diwanshu.suman@gmail.com>
2023-08-25 12:32:39 +0530

    commit

--- controller/viewControllers.js
+++ controller/viewControllers.js
@@ -1,12 +1,16 @@
 import Game from "../model/Game.js";
 import PlayIO from "../socketio/PlayNamespace.js";
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 
 const homePage = (req, res) => {
   let obj={user: req.user};
   console.log(obj)
+  // base_URL="http://localhost:3000"
+
   if(req.user)
   {
-    fetch(`http://localhost:3000/api/v1/allgames/${req.user.userId}`).then(res=>res.json()).then(games=>{
+    fetch(`${process.env.base_URL}/api/v1/allgames/${req.user.userId}`).then(res=>res.json()).then(games=>{
 console.log(games)
     res.render("index", { user: req.user,games:games.allgames });
   }).catch(error=>{
